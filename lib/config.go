6ae9dd0dbdb22cbb917f7133b6bac89b5c413e0d
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-04-03 11:36:38 -0600

    no

--- lib/config.go
+++ lib/config.go
@@ -3,8 +3,6 @@ package lib
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 )
 
@@ -15,15 +13,6 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	fmt.Println(os.Getwd())
-	files, err := ioutil.ReadDir("../")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}
 	file, err := os.Open("../config.json")
 
 	if err != nil {
