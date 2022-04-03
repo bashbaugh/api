5883c3981521ad27fe3bef7b4ceefad563b43d17
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-04-03 13:14:27 -0600

    cors

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/bashbaugh/api/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -13,6 +15,15 @@ func main() {
 	godotenv.Load()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           2 * time.Hour,
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.Redirect(http.StatusTemporaryRedirect, "https://github.com/bashbaugh/api")
