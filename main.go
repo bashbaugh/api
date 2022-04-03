5e84c94faaa40d44435441f738f42a1926171e50
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2022-04-02 23:16:11 -0600

    vercel

--- main.go
+++ main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	// "net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/jason0x43/go-toggl"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-
-	r := gin.Default()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	r.GET("/now", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"taskName": getCurrentTogglEntry().Description,
-		})
-	})
-
-	r.Run("0.0.0.0:4000")
 }
 
 func getCurrentTogglEntry() toggl.TimeEntry {
